14e70dd3aa52d4f935f652d6afc6ddb2488ac112
itamarKraitman <itamar.kraitman@msmail.ariel.ac.il>
2022-12-06 13:03:05 +0200

    need to debug join

--- src/variableElimination.java
+++ src/variableElimination.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Implementing probability inference using Variable Elimination algorithm
@@ -100,6 +101,9 @@ public class variableElimination implements Comparator<ArrayList<HashMap<String,
             ArrayList<HashMap<String, String>> hiddenFactorAfterJoin = join(hiddenVar, hiddenFactors);
 
             // eliminate hidden
+            ArrayList<HashMap<String, String>> hiddenFactorAfterEliminate = eliminate(hiddenVar, hiddenFactorAfterJoin);
+
+            // delete the factor if it's one value
 
 
         }
@@ -128,6 +132,53 @@ public class variableElimination implements Comparator<ArrayList<HashMap<String,
      * @return the joined factor
      */
     private ArrayList<HashMap<String, String>> join(BayesianNetworkNode hiddenToJoin, ArrayList<ArrayList<HashMap<String, String>>> hiddenFactors) {
+        // joining hidden factors until only one factor left
+        while (hiddenFactors.size() > 1) {
+            ArrayList<HashMap<String, String>> firstFactor = hiddenFactors.get(0);
+            ArrayList<HashMap<String, String>> secondFactor = hiddenFactors.get(1);
+            ArrayList<HashMap<String, String>> newFactor = new ArrayList<>();
+            newFactor.add(new HashMap<>());
+            newFactor.get(0).putAll(firstFactor.get(0));
+            newFactor.get(0).putAll(secondFactor.get(0));
+            // adding all the variables present in both factors to the set for later use in multiplication process
+            Set<String> variablesPresentInBoth = firstFactor.get(0).keySet().stream().filter(key -> secondFactor.get(0).containsKey(key)).collect(Collectors.toSet());
+
+            // iterating along firstFactor, and for each line find the line in secondFactor that matches the outcomes of the common variables
+            // when found, multiply theirs probability and add to the new factor
+            boolean firstFirstLine = true;
+            for (HashMap<String, String> firstFactorLine : firstFactor) {
+                if (firstFirstLine) { // no need to check first line (eg A->A)
+                    firstFirstLine = false;
+                    continue;
+                }
+                boolean secondFirstLine = true;
+                for (HashMap<String, String> secondFactorLine : secondFactor) {
+                    if (secondFirstLine) {
+                        secondFirstLine = false;
+                        continue;
+                    }
+                    boolean linesMatches = true;
+                    // check if the outcomes of common are equal. if not, skip to the next line
+                    for (String key : variablesPresentInBoth) {
+                        if (!firstFactorLine.get(key).equals(secondFactorLine.get(key))) {
+                            linesMatches = false;
+                            break;
+                        }
+                    }
+                    HashMap<String, String> newFactorLine = new HashMap<>();
+                    // add all keys to the line, common variables outcomes are equal so no need to check again
+                    for (String key : firstFactorLine.keySet())
+                        newFactorLine.put(key, firstFactorLine.get(key));
+                    secondFactorLine.keySet().forEach(key -> newFactorLine.put(key, secondFactorLine.get(key)));
+                    // calculate the probability and put it in the new line
+                    double multiplyProbability = Double.parseDouble(firstFactorLine.get("prob")) * Double.parseDouble(secondFactorLine.get("prob"));
+                    newFactorLine.put("prob", String.valueOf(multiplyProbability));
+                    multiplications++;
+                    newFactor.add(newFactorLine);
+                }
+            }
+
+        }
         return null;
     }
 

9f38064a9fef654d8954051ebe0a584a80dfd6ba
itamarKraitman <itamar.kraitman@msmail.ariel.ac.il>
2022-12-12 11:00:18 +0200

    add reading another xml file during execution

--- src/Ex1.java
+++ src/Ex1.java
@@ -10,7 +10,6 @@ public class Ex1 {
 //        System.out.println("java version: " + version);
 //        String networkPath = "src/"; // for reading the net name
         String networkPath = ""; // for cmd
-        ArrayList<String> queries = new ArrayList<>();
         try {
             // remember to remove src/ before running from the cmd
 //            FileReader fr = new FileReader("src/input2.txt"); // dor debug
@@ -25,6 +24,11 @@ public class Ex1 {
             String query = br.readLine();
 
             while (query != null) {
+                if (query.endsWith("xml")) { // read another xml input
+//                    network = ReadNetFromXmlFile.readNetFromXml("src/" +query); // for debug
+                    network = ReadNetFromXmlFile.readNetFromXml(query); // for cmd
+                    query = br.readLine();
+                }
                 String answer = "";
                 double[] result = new double[3];
                 String[] querySplit = query.split("\\),");
@@ -40,7 +44,7 @@ public class Ex1 {
                         break;
                 }
                 answer += (result[0] + "," + (int) result[1] + "," + (int) result[2]);
-                bw.write(answer+"\n");
+                bw.write(answer + "\n");
                 query = br.readLine();
             }
             br.close();
